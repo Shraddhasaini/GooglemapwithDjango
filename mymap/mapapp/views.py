9bd0823984c7cb40907597a252f6ec9837ae1f8c
Shraddhasaini <shraddhasaini99@gmail.com>
2020-04-05 17:50:36 +0530

    updated views

--- mymap/mapapp/views.py
+++ mymap/mapapp/views.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.shortcuts import render
+#from __future__ import unicode_literals
+from django.http import HttpResponse
+from django.template import loader
+
 from .models import Rental
 
 
 def index(request):
-    context = {latitude: -20.344, longitude: 131.036}
-    return render(request, 'mapapp/index.html', context)
+    template = loader.get_template('mapapp/index.html')
+    context = {
+        'latitude': -20.344, 'longitude': 131.036,
+    }
+    return HttpResponse(template.render(context, request))
